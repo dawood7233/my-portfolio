4183044d3fa7968eb29cecacbd97660b0487bf4a
dawood <m.dawood7233@gmail.com>
2024-01-31 16:39:05 +0500

    resume done

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,16 +1,97 @@
-import React from 'react'
+import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
+import { resumeData } from "./utils";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import "./style.scss"
+import { MdOutlineLaptopChromebook } from "react-icons/md";
+import { FaBook } from "react-icons/fa6";
 
 const Resume = () => {
   return (
     <section id="resume" className="resume">
-    <PageHeaderContent
-      headerText="Resume"
-      icon={<BsInfoCircleFill size={40} />}
-    />
-  </section>
-  )
-}
+      <PageHeaderContent
+        headerText="Resume"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="timeline">
+        <div className="timeline__experience">
+          <h3 className="timeline__experience__text">Experience</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--green-theme-main-color)"
+          >
+            {resumeData.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background:'none',
+                  color: "var(--green-theme-sub-text-color)",
+                  border: "1.5px solid var(--green-theme-main-color)"
+                }}
+                date="2024 - present"
+                icon={<MdOutlineLaptopChromebook/>}
+                iconStyle={{
+                  background:"#181818",
+                  color: "var(--green-theme-main-color)"
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>
+                    {item.title}
+                  </h3>
+                  <h4>
+                    {item.subTitle}
+                  </h4>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">{item.description}</p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline__education">
+          <h3 className="timeline__education__text">Education</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--green-theme-main-color)"
+          >
+          {resumeData.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background:'none',
+                  color: "var(--green-theme-sub-text-color)",
+                  border: "1.5px solid var(--green-theme-main-color)"
+                }}
+                date="2024 - present"
+                icon={<FaBook/>}
+                iconStyle={{
+                  background:"#181818",
+                  color: "var(--green-theme-main-color)"
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>
+                    {item.title}
+                  </h3>
+                  <h4>
+                    {item.subTitle}
+                  </h4>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">{item.description}</p>
+              </VerticalTimelineElement>
+            ))}
+            </VerticalTimeline>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Resume
+export default Resume;
---
+++ src/pages/resume/utils.js
@@ -0,0 +1,35 @@
+export const resumeData ={
+    experience:[
+        {
+            title: "MERN Stack Trainee",
+            subTitle: "Knowledge Streams",
+            description: "Networking , Soft Skills"
+
+        },
+        {
+            title : "UI/UX Internship",
+            subTitle: "CODSOFT",
+            description : "I have learned about the user-experience and user-interface in this internship. It's been a wonderful time to learn."
+        }
+    ],
+    education:[
+        {
+            title : "Secondary",
+            subTitle: "Wapda boys high school Lahore",
+            description: "i have done my matriculation in computer science in 2016",
+        },
+        {
+            title : "Higher Secondary",
+            subTitle: "Gov.t shalimar college Lahore",
+            description: "I attained my higher secondary education in computer science. This program provided me with a robust knowledge base in computer science, encompassing essential concepts, programming languages, and problem-solving techniques. This education has equipped me with a solid foundation for further academic and professional pursuits in the field.",
+        },
+        {
+            title : "Graduation",
+            subTitle: "University of Education",
+            description: "I am presently pursuing my undergraduate degree in Information Technology at the University of Education. This program is expanding my knowledge of modern technologies and programming, preparing me to meet the challenges of the evolving IT industry. I am eager to continue advancing my skills in this dynamic field through my ongoing education.",
+        },
+        
+        
+
+    ]
+}
